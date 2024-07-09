5b3e9f5392f163423d0b72d9d7a9a89fe43224ea
Andrey <Primer@mail.ru>
2024-07-09 17:22:24 +0500

    Добавил html base, index, начал добавлять модель User

--- myproject/nuts_shop/models.py
+++ myproject/nuts_shop/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.forms import forms
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя клиента')
+    password = models.CharField(max_length=48, widget=forms.PasswordInput())
+    email = models.EmailField(verbose_name='Почта')
+    phone = models.CharField(max_length=12, verbose_name='Телефон')
+    address = models.CharField(max_length=150, verbose_name='Адрес')
+    date_registered = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
